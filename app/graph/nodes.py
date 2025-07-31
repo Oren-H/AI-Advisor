d4ebdc147041a401d4347144d010f5bcf7ca92f1
Oren Hartstein <orenhartstein@quelements.com>
2025-07-31 13:20:44 -0700

    modified Specific agent

--- app/graph/nodes.py
+++ app/graph/nodes.py
@@ -96,7 +96,7 @@ def search_courses_node(state: CourseAdvisorState) -> CourseAdvisorState:
         course_results = query_courses_with_filters(
             state['user_query'], 
             filters=state['filters'], 
-            k=5,
+            k=15,
             conversation_context=conversation_context
         )
         
